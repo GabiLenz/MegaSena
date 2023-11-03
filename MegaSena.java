375cb75652da3f89b621ae3575c08af3a2ca76c6
GabiLenz <135671513+GabiLenz@users.noreply.github.com>
2023-11-02 21:50:27 -0300

    Add files via upload

---
+++ MegaSena.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class MegaSena{
+
+   public static void main(String [] agrs ) {
+         Scanner scanner  = new scanner (System.in);
+
+sysout("Digite seu nome");
+String name  = scanner.nextLine();
+sysout ("Ola, " + name);
+
+}
+
+}
+
